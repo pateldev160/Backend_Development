da9b1544e27e72ee2bcce49588f5a863a08d78b2
DevPatel016 <pateldev014@gmail.com>
2024-03-13 18:02:46 -0400

    curd-read

---
+++ CRUD_Read/src/main/java/com/pateldev/CrudReadApplication.java
@@ -0,0 +1,13 @@
+package com.pateldev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudReadApplication.class, args);
+    }
+
+}
---
+++ CRUD_Read/src/test/java/com/pateldev/CrudReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pateldev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
